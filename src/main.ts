03685e04097f94404b2b53854781e509feaa0b31
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-02-03 12:36:23 +0100

    all done

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import {initiateModel, detectObjects} from "./modelConfig"
+
+const statusText = document.createElement("p")
+statusText.textContent = "Loading Model..."
+
 const resultCont = document.getElementById("result-container") as HTMLDivElement
 const video = document.getElementById("video-element") as HTMLVideoElement
 let currentStream: MediaStream | null = null
@@ -10,14 +14,20 @@ const startCamera = async () => {
     })
     video.srcObject = userVideo 
     currentStream = userVideo
+    
+    return new Promise((resolve) => {
+      video.onloadedmetadata = () => resolve(null)
+    })
+    
   }catch(error){
     console.error("Error accessing user camera", error)
   }
 }
 
+resultCont.appendChild(statusText);
 
 
-startCamera().then(async () => {
-  const model = await initiateModel()
+Promise.all([startCamera(), initiateModel()]).then(([_, model]) => {
+  resultCont.innerHTML = ""
   detectObjects(model, video, resultCont, currentStream)
 })
--- src/modelConfig.ts
+++ src/modelConfig.ts
@@ -1,8 +1,5 @@
 let detectionInterval: number | null = null
 
-const statusText = document.createElement("p")
-statusText.textContent = "Processing......"
-
 export const initiateModel = async (): Promise<cocoSsd.ObjectDetection> => {
   const model = await cocoSsd.load()
   return model 
@@ -10,19 +7,15 @@ export const initiateModel = async (): Promise<cocoSsd.ObjectDetection> => {
 
 export const detectObjects = async (model: cocoSsd.ObjectDetection, video: HTMLVideoElement, resultCont: HTMLDivElement, currentStream: MediaStream | null) => {
   const detect = async () => {
-    if(resultCont && !resultCont.contains(statusText)) {
-      resultCont.innerHTML = ""
-      resultCont.appendChild(statusText)
-    }
-   
     const results = await model.detect(video)
-    resultCont.innerHTML = ""
     
     if(results.length === 0){
       const noResult = document.createElement("p")
       noResult.textContent = "No Recognizable Object Detected"
+      resultCont.innerHTML = ""
       resultCont.appendChild(noResult)
     }else{
+    resultCont.innerHTML = ""
     results.forEach((r) => {
       const resultEl = document.createElement("p")
       resultEl.textContent = `${r?.class} - ${(r?.score * 100).toFixed(2)}%`
